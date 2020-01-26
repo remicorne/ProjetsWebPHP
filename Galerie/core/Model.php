c7932efae7b79a06f7eb1f437749960eb4c3bf96
remicorne <remi.corne@gmail.com>
2020-01-26 10:20:02 +0100

    commit avant l'ajout de la BD

--- Galerie/core/Model.php
+++ Galerie/core/Model.php
@@ -1,4 +1,17 @@
 <?php
 class Model {
+  private static $static_db;
+  protected $db;
+
+  public function __construct() {
+    $this->db = self::$static_db;
+  }
+
+  public static function init() {
+    global $config;
+    self::$static_db = new PDO('sqlite:models/database.sqlite');
+  }
 }
+
+Model::init();
 ?>
